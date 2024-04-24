887fb7530f08c3465f5bc953c80797f55f4095c7
Thomas Fossati <thomas.fossati@linaro.org>
2024-04-24 15:52:00 +0000

    hook it up to the veraison deployment
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- go.mod
+++ go.mod
@@ -4,27 +4,30 @@ go 1.22.2
 
 require (
 	github.com/google/go-configfs-tsm v0.2.2
-	github.com/veraison/ccatoken v1.1.0
+	github.com/lestrrat-go/jwx/v2 v2.0.8
+	github.com/veraison/apiclient v0.2.0
+	github.com/veraison/ear v1.1.2
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/huandu/xstrings v1.3.3 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
 	github.com/lestrrat-go/httprc v1.0.4 // indirect
 	github.com/lestrrat-go/iter v1.0.2 // indirect
-	github.com/lestrrat-go/jwx/v2 v2.0.8 // indirect
 	github.com/lestrrat-go/option v1.0.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
-	github.com/veraison/eat v0.0.0-20220117140849-ddaf59d69f53 // indirect
-	github.com/veraison/go-cose v1.0.0-rc.1 // indirect
-	github.com/veraison/psatoken v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/moogar0880/problems v0.1.1 // indirect
+	github.com/veraison/cmw v0.1.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/oauth2 v0.11.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,35 +1,76 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
 	"github.com/google/go-configfs-tsm/configfs/linuxtsm"
 	"github.com/google/go-configfs-tsm/report"
-	"github.com/veraison/ccatoken"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/veraison/apiclient/verification"
+	"github.com/veraison/ear"
 )
 
+type TSMEvidenceBuilder struct{}
+
+func (eb TSMEvidenceBuilder) BuildEvidence(nonce []byte, accept []string) ([]byte, string, error) {
+	for _, ct := range accept {
+		if ct == "application/eat-collection; profile=http://arm.com/CCA-SSD/1.0.0" {
+			req := &report.Request{
+				InBlob: nonce,
+			}
+
+			res, err := linuxtsm.GetReport(req)
+			if err != nil {
+				return nil, "", fmt.Errorf("GetReport failed: %s", err)
+			}
+
+			return res.OutBlob, ct, nil
+		}
+	}
+
+	return nil, "", errors.New("no match on accepted media types")
+}
+
 func main() {
-	req := &report.Request{
-		InBlob: []byte("random-challenge"),
+	cfg := verification.ChallengeResponseConfig{
+		NonceSz:         64,
+		EvidenceBuilder: TSMEvidenceBuilder{},
+		NewSessionURI:   "http://veraison.example:8080/challenge-response/v1/newSession",
+		DeleteSession:   true,
 	}
 
-	res, err := linuxtsm.GetReport(req)
+	ar, err := cfg.Run()
 	if err != nil {
-		log.Fatalf("GetReport failed: %s", err)
+		log.Fatalf("Veraison API client session failed: %v", err)
 	}
 
-	e := ccatoken.Evidence{}
+	if err := processAR(ar[1 : len(ar)-1]); err != nil {
+		log.Fatalf("EAR processing failed: %v", err)
+	}
+}
 
-	err = e.FromCBOR(res.OutBlob)
-	if err != nil {
-		log.Fatalf("CCA token CBOR decoding failed: %s", err)
+func processAR(ares []byte) error {
+	earVerificationKey := `{
+		"alg": "ES256",
+		"crv": "P-256",
+		"kty": "EC",
+		"x": "usWxHK2PmfnHKwXPS54m0kTcGJ90UiglWiGahtagnv8",
+		"y": "IBOL-C3BttVivg-lSreASjpkttcsz-1rb7btKLv8EX4"
+	}`
+
+	vfyK, _ := jwk.ParseKey([]byte(earVerificationKey))
+
+	var ar ear.AttestationResult
+
+	if err := ar.Verify(ares, jwa.ES256, vfyK); err != nil {
+		return err
 	}
 
-	b, err := e.MarshalJSON()
-	if err != nil {
-		log.Fatalf("CCA token JSON marshalling failed: %s", err)
-	}
+	j, _ := ar.MarshalJSONIndent("", " ")
+	fmt.Println(string(j))
 
-	fmt.Println(string(b))
+	return nil
 }
